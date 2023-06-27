6f146df3d653b09d6744c79caebf6981fdf2c32d
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-06-26 23:15:47 -0300

    feat: change the color text on click tabs

--- src/components/Tabs/tabs.component.tsx
+++ src/components/Tabs/tabs.component.tsx
@@ -30,6 +30,8 @@ export function Tabs({ tabs }: TabComponentProps) {
             css={{
               background:
                 activeTab === index ? "$gradientButtonPurple" : "transparent",
+              color:
+                activeTab === index ? "white" : "black",
             }}>
             {tab.title}
           </S.TabItem>
